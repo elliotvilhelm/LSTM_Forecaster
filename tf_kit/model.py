80e548f03d4a4354e2a7b890e92392d0dace01a4
Elliot V Pourmand <elliot@pourmand.com>
2020-09-06 10:20:06 -0700

    bidirectional lstm
    :

--- tf_kit/model.py
+++ tf_kit/model.py
@@ -9,14 +9,17 @@ def get_lstm():
     """
     shape = (HISTORY_SIZE, len(FEATURES))
     ssm = tf.keras.models.Sequential()
-    ssm.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128, return_sequences=True,
+    ssm.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(12, return_sequences=False,
                                                                input_shape=shape)))
 
-    ssm.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128)))
+    # ssm.add(tf.keras.layers.Bidirectional(tf.keras.layers.LSTM(128)))
 
-    ssm.add(tf.keras.layers.Dense(64, activation='relu'))
+    ssm.add(tf.keras.layers.Dense(16, activation='relu'))
     ssm.add(tf.keras.layers.Dense(N_CLASSES, activation='softmax'))
     ssm.compile(optimizer=tf.keras.optimizers.Adam(lr=0.001),
                 loss=tf.keras.losses.CategoricalCrossentropy(),
                 metrics=['accuracy', tfa.metrics.F1Score(num_classes=N_CLASSES)])
     return ssm
+
+
+
